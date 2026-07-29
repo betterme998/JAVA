1b8fd7f11172fb4cfcdc117ee5f4960b862e2d5e
betterme998 <betterme998@163.com>
2026-07-29 16:53:29 +0800

    .

--- 1_JAVA基础/1.5变量原理/Var01.java
+++ 1_JAVA基础/1.5变量原理/Var01.java
@@ -59,7 +59,7 @@ public class Var01 {
     // 3.课堂测试题 1min
     System.out.println(100 + 98); //198
     System.out.println("100" + 98);//10098
-    System.out.printin(100 + 3 + "hello");//103hello;
+    System.out.println(100 + 3 + "hello");//103hello;
     System.out.println("hello" + 100 + 3);//hello1003;
 
     // 数据类型
