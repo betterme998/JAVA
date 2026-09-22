9d2cbb0d368fc9d767f8053822a25b5624791c94
betterme998 <betterme998@163.com>
2026-09-22 17:02:19 +0800

    循环

--- 1_JAVA基础/2.0控 制 结 构/MulForExercise.java
+++ 1_JAVA基础/2.0控 制 结 构/MulForExercise.java
@@ -32,9 +32,10 @@ public class MulForExercise {
 
     // 2.打印出九九乘法表[课后题]
     for(int i = 1; i <=9; i++) {
-      for(int j = i; j <= 9; j++) {
-        System.out.println(i + "*" + j + "=" + i * j + "\t");
+      for(int j = 1; j <= i; j++) {
+        System.out.print(j + "*" + i + "=" + i * j + "\t" + " ");
       }
+      System.out.println();
     }
   }
 }
